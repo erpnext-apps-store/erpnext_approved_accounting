695a893b78b7dd2a29a457c9157d1a346aa363d8
Massimo Fierro <massimo.fierro@one-asset.co.kr>
2019-11-18 09:50:52 +0900

    Added further necesary doctypes

---
+++ approved_accounting/approved_accounting/doctype/approved_purchase_order/approved_purchase_order.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, One Asset Management and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+# import frappe
+from erpnext.erpnext.buying.doctype.purchase_order.purchase_order import PurchaseOrder
+
+class ApprovedPurchaseOrder(PurchaseOrder):
+	def __init__(self, *args, **kwargs):
+		super(ApprovedPurchaseOrder, self).__init__(*args, **kwargs)
+
+	def validate(self):
+		super(ApprovedPurchaseOrder, self).validate()
+`
